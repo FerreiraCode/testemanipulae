e3af8058368df7e7216bc26a871db1379c626a99
Thiago Ferreira <Thiagofsaf@gmail.com>
2023-11-03 15:07:33 -0300

    Conclusão do E2E

---
+++ cypress/cypress/e2e/manipulae.cy.js
@@ -0,0 +1,30 @@
+import * as variaveis from '../support/variaveis';
+import 'cypress-file-upload';
+const axios = require('axios');
+const fs = require('fs');
+
+describe('EnvioDeReceita', () => {
+  it('Receita', () => {
+
+    cy.visit(variaveis.url)
+
+    cy.get('input[type="file"]').should('exist');
+    cy.get('input[type="file"]').attachFile(variaveis.filePath);
+  
+    cy.get('input[placeholder="00000-000"]').should('exist');
+    cy.get('input[placeholder="00000-000"]').type(variaveis.novoCep);
+    cy.get('input[name="email"]').type(variaveis.email);
+    cy.get('input[name="name"]').type(variaveis.nome);
+    cy.get('input[type="tel"]').type(variaveis.numeroTelefone);
+
+    cy.get('button[type="submit"]').should('exist');
+    cy.get('span[aria-hidden="true"]').click();
+    cy.get('button[type="submit"]').click();
+   
+
+
+    cy.contains('Receita enviada').timeout(10000).should('exist');
+
+
+  });
+});
---
+++ cypress/cypress/support/variaveis.js
@@ -0,0 +1,7 @@
+
+export const url = 'https://webmani-test.manipulae.com.br/cotar';
+export const filePath = '\manipulae/ReceitaDefinitiva.pdf';
+export const novoCep = '64039-080';
+export const email = 'email@examplo.com';
+export const nome = 'Teste Thiago';
+export const numeroTelefone = '12345678901';
